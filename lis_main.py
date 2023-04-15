28364e4d29cad6d9ab3c0ac9f33e3ea9ea9846b4
kjbn89 <kevin.jonathan.binder@mni.thm.de>
2023-04-15 14:14:05 +0200

    added basic event system (BaseEvent, EventDistributor, EventReceiver)

--- lis_main.py
+++ lis_main.py
@@ -5,6 +5,7 @@ import asyncio
 import os
 from dotenv import load_dotenv
 
+from core_modules.eventing.EventDistributor import EventDistributor
 from core_modules.rest.PingApi import PingApi
 from core_modules.rest.RestServer import RestServer
 
@@ -22,9 +23,11 @@ async def main():
     Main Coroutine that gets run on the event loop.
     """
     load_dotenv("lis.env")
+    event_distributor = EventDistributor()
     rest_server = RestServer()
     rest_server.register_apis([PingApi()])
     rest_server.start_server(os.getenv("SERVER_IP"), int(os.getenv("SERVER_PORT")))
+
     await never_ending_function()
     # never reached!
 
---
+++ src/core_modules/eventing/BaseEvent.py
@@ -0,0 +1,5 @@
+class BaseEvent:
+    """
+    Base class every event needs to extend to be distributable via the EventDistributor
+    """
+    ...
